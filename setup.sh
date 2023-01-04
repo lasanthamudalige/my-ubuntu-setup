3c5fbc47ea395d905bf6e222fcb4d3e88592c8da
lasantha <lasantha1130@gmail.com>
2023-01-04 17:15:23 +0530

    add check for fonts folder in home and enter '1' to accept eula in chrome install

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 # Refresh repos and install git, curl, vlc, ubuntu-restricted-extras and build-essential
-sudo apt update && sudo apt install git curl vlc ubuntu-restricted-extras build-essential python3-pip python3-tk -y
+sudo apt update && sudo apt install git curl vlc ubuntu-restricted-extras build-essential python3-pip python3-tk -y # Enter 'y' to download
 
 # Install deb-get
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
 # Install software from deb-get
-sudo deb-get install google-chrome-stable code spotify-client
+sudo deb-get install google-chrome-stable code spotify-client -'1' # Enter 1 to accept EULA from google chrome
 
 # Download and install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -15,10 +15,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Generate new ssh key for github
-'\n\n\n' | ./generate_new_ssh_key.sh
+"\n"\n"\n" | ./generate_new_ssh_key.sh
 
 # Create a font directory inside home
-mkdir ~/.fonts
+DIR=~/.fonts
+if [ ! -d "$DIR" ];
+then
+	sudo mkdir ~/.fonts
+fi
 
 # Add fonts to home directory
 sudo cp -r font/Fira_Code_v6.2 font/MesloLGS-NF ~/.fonts
