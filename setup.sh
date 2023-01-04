dfbfd65c34e4d696af5b5a1e27d901e5d88685bc
lasantha <lasantha1130@gmail.com>
2023-01-04 19:37:34 +0530

    add press 1 to accept eula in chrome install and press enter to 3 inputs after ssh generation

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 # Refresh repos and install git, curl, vlc, ubuntu-restricted-extras and build-essential
-sudo apt update && sudo apt install git curl vlc ubuntu-restricted-extras build-essential python3-pip python3-tk -y # Enter 'y' to download
+sudo apt update && sudo apt install ubuntu-restricted-extras git curl vlc build-essential python3-pip python3-tk -y # Enter 'y' to download
 
 # Install deb-get
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
 # Install software from deb-get
-sudo deb-get install google-chrome-stable code spotify-client -'1' # Enter 1 to accept EULA from google chrome
+echo '1' | sudo deb-get install google-chrome-stable code spotify-client
 
 # Download and install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -13,9 +13,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# Generate new ssh key for github
-"\n"\n"\n" | ./generate_new_ssh_key.sh
 
 # Create a font directory inside home
 DIR=~/.fonts
@@ -26,3 +23,6 @@ fi
 
 # Add fonts to home directory
 sudo cp -r font/Fira_Code_v6.2 font/MesloLGS-NF ~/.fonts
+
+# Generate new ssh key for github
+echo | ./generate_new_ssh_key.sh # Press enter to 3 questions after creating ssh key
