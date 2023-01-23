8a77f95a518a34a7cee5d34d2ffc133b266d61d9
Lasantha <lasantha1130@gmail.com>
2023-01-23 20:22:04 +0530

    remove enter when for new ssh key generation

--- install.sh
+++ install.sh
@@ -28,4 +28,4 @@ sudo cp -r font/Fira_Code_v6.2 font/MesloLGS-NF ~/.fonts
 fc-cache -rv
 
 # Generate new ssh key for github
-echo | ./generate_new_ssh_key.sh # Press enter to 3 questions after creating ssh key
+./generate_new_ssh_key.sh 
