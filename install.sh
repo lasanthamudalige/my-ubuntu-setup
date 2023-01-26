7be67f8f13c827119cf0690697041f17ee30a0d2
Lasantha <lasantha1130@gmail.com>
2023-01-26 22:19:55 +0530

    move nvm install to top

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
+# Download and install nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
 # Refresh repos and install git, curl, vlc, ubuntu-restricted-extras and build-essential
 sudo apt update && sudo apt install ubuntu-restricted-extras vim curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk tlp -y # Enter 'y' to download
 
 # Remove power demon to avoid any conflict with tlp
 sudo apt remove power-profiles-daemon
 
-
 # Install deb-get
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
 # Install software from deb-get
 sudo deb-get install google-chrome-stable code spotify-client
-
-# Download and install nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # Create a font directory inside home
 DIR=~/.fonts
