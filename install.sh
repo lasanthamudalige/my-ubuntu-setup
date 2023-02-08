a9610e66e5a8021d7508726d393a087a68d01c9f
Lasantha <lasantha1130gmail.com>
2023-02-08 23:35:16 +0530

    move install deb-get to top

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
+# Install deb-get
+curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+
 # Refresh repos and install git, curl, vlc, ubuntu-restricted-extras and build-essential
 sudo apt update && sudo apt install ubuntu-restricted-extras vim curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk tlp -y # Enter 'y' to download
 
 # Remove power demon to avoid any conflict with tlp
 sudo apt remove power-profiles-daemon
-
-# Install deb-get
-curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
 # Install software from deb-get
 sudo deb-get install google-chrome-stable code spotify-client
