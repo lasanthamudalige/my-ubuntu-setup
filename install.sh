e9dbf3bddeb0317c76ce83e840201e1b915ec7a7
Lasantha <lasantha1130@gmail.com>
2023-02-23 14:10:13 +0530

    add git

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
 # Refresh repos and install git, curl, vlc, ubuntu-restricted-extras and build-essential
-sudo apt update && sudo apt install ubuntu-restricted-extras vim curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk tlp -y # Enter 'y' to download
+sudo apt update && sudo apt install ubuntu-restricted-extras vim git curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk tlp -y # Enter 'y' to download
 
 # Remove power demon to avoid any conflict with tlp
 sudo apt remove power-profiles-daemon
