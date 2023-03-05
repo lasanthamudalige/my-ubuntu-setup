24ef663daf713a55e3b61eb8253e330b2a98fb75
Lasantha <lasantha1130@gmail.com>
2023-03-05 18:42:47 +0530

    remove kitty terminal and add jetbrainsmono nerd font install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
 # Refresh repos and install git, curl, vlc, ubuntu-restricted-extras and build-essential
-sudo apt update && sudo apt install ubuntu-restricted-extras fish kitty htop neofetch vim git curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk tlp -y # Enter 'y' to download
+sudo apt update && sudo apt install ubuntu-restricted-extras fish htop neofetch vim git curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk tlp -y # Enter 'y' to download
 
 # Remove power demon to avoid any conflict with tlp
 sudo apt remove power-profiles-daemon
@@ -13,15 +13,8 @@ sudo apt remove power-profiles-daemon
 # Install software from deb-get
 sudo deb-get install google-chrome-stable code spotify-client
 
-# Create a font directory inside home
-DIR=~/.fonts
-if [ ! -d "$DIR" ];
-then
-	sudo mkdir ~/.fonts
-fi
-
-# Add fonts to home directory
-sudo cp -r font/Fira_Code_v6.2 font/MesloLGS-NF ~/.fonts
+# Install JetbrainsMono NerdFont
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 
 # Update font cache
 fc-cache -rv
