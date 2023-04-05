c23aeea5095b9ca9ed1b7955012928c9a5025a57
Lasantha Mudalige <91461938+lasanthamudalige@users.noreply.github.com>
2023-04-05 20:10:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
 
 # Refresh repos and install git, curl, vlc, ubuntu-restricted-extras and build-essential
-sudo apt update && sudo apt install ubuntu-restricted-extras fish htop neofetch vim git curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk -y # Enter 'y' to download
+sudo apt update && sudo apt install ubuntu-restricted-extras fish htop neofetch vim git curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk obs-studio -y # Enter 'y' to download
 
 # Remove power demon to avoid any conflict with tlp
 sudo apt remove power-profiles-daemon
 
 # Install software from deb-get
-sudo deb-get install brave-browser code spotify-client obs-studio
+sudo deb-get install brave-browser code spotify-client
 
 # Install JetbrainsMono NerdFont
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
