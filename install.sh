11fe22213e28d6b414c93ad9171a5be8279b63d9
Lasantha <lasantha1130@gmail.com>
2023-04-27 08:38:56 +0530

    fix firefox not installing

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ Pin-Priority: 1001
 # This to ensure firefox automatic updates
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 # Install firefox apt version
-sudo apt update && install firefox -y
+sudo apt update && sudo apt install firefox -y
 
 # Refresh repos and install git, curl, vlc, ubuntu-restricted-extras and build-essential
 sudo apt install ubuntu-restricted-extras fish htop neofetch neovim git curl gnome-clocks gnome-dictionary vlc build-essential python3-pip python3-tk obs-studio -y # Enter 'y' to download
